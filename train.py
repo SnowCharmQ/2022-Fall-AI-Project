8bb9b21089b6832df413e9177ee84db0b4de3ac3
SnowCharm <619022098@qq.com>
2022-10-18 21:56:28 +0800

    initialization test for random idiot

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from ai import *
+from idiot import *
 from concurrent.futures import *
 
 
@@ -14,23 +15,44 @@ class Gene:
 
 def evaluate(g1: Gene, g2: Gene):
     cnt = 0
-    cnt += battle(g1, g2)
-    cnt -= battle(g2, g1)
+    ai_black = AI(8, COLOR_BLACK, 5, g1.data)
+    ai_white = AI(8, COLOR_WHITE, 5, g2.data)
+    cnt += battle_against(ai_black, ai_white)
+    ai_black = AI(8, COLOR_BLACK, 5, g2.data)
+    ai_white = AI(8, COLOR_WHITE, 5, g1.data)
+    cnt -= battle_against(ai_black, ai_white)
     if cnt == 2:
-        g1.fitness += 6
-        g2.fitness -= 4
+        g1.fitness += 10
     elif cnt == 1:
-        g1.fitness += 3
-        g2.fitness -= 1
+        g1.fitness += 7
+        g2.fitness += 2
     elif cnt == -1:
-        g2.fitness += 3
-        g1.fitness -= 1
+        g2.fitness += 7
+        g1.fitness += 2
     elif cnt == -2:
-        g2.fitness += 6
-        g1.fitness -= 4
+        g2.fitness += 10
     else:
-        g1.fitness += 1
-        g2.fitness += 1
+        g1.fitness += 4
+        g2.fitness += 4
+
+
+def get_valid_gene():
+    while 1:
+        data = []
+        for pos in range(len(low) - 2):
+            data.append(random.randint(bound[0][pos], bound[1][pos]))
+        data.append(random.random())
+        data.append(random.random())
+        g = Gene(data)
+        cnt = 0
+        ai_black = AI(8, COLOR_BLACK, 5, g.data)
+        ai_white = Idiot(8, COLOR_WHITE, 5)
+        cnt += battle_against(ai_black, ai_white)
+        ai_black = Idiot(8, COLOR_BLACK, 5)
+        ai_white = AI(8, COLOR_WHITE, 5, g.data)
+        cnt -= battle_against(ai_black, ai_white)
+        if cnt == 2:
+            return g
 
 
 def get_winner(chessboard):
@@ -50,9 +72,7 @@ def judge(chessboard):
     return False
 
 
-def battle(g1, g2):
-    ai_black = AI(8, COLOR_BLACK, 5, g1.data)
-    ai_white = AI(8, COLOR_WHITE, 5, g2.data)
+def battle_against(ai_black, ai_white):
     board = np.zeros((8, 8))
     board[3][3] = COLOR_WHITE
     board[4][4] = COLOR_WHITE
@@ -109,10 +129,10 @@ def get_fitness(population: list):
 
 low = -500
 low = [low for _ in range(30)]
-low.extend([0, 0, low])
+low.extend([0, 0, low, 1])
 high = 100
 high = [high for _ in range(30)]
-high.extend([1, 1, high])
+high.extend([1, 1, high, 10])
 bound = (low, high)
 cr_p = 0.2
 mut_p = 0.2
@@ -124,19 +144,25 @@ pop_size = 16
 init_data = [-500, 25, -10, 45, -1, -5, -3, -2, -1, -1,
              -500, 25, -10, 45, -1, -5, -3, -2, -1, -1,
              -500, 25, -10, 45, -1, -5, -3, -2, -1, -1,
-             0.1, 0.5, 1, 1]
+             1, 1, 0.1, 0.5]
+init_gene = Gene(init_data)
+pop.append(init_gene)
+init_data = [-70, 5, -2, 10, -5, -2, -1, -1, -1, -2,
+             -70, 5, -2, 10, -5, -2, -1, -1, -1, -2,
+             -70, 5, -2, 10, -5, -2, -1, -1, -1, -2,
+             1, 1, 0.05, 0.5]
 init_gene = Gene(init_data)
 pop.append(init_gene)
 s = time.time()
-for _ in range(pop_size - 1):
-    data = []
-    for pos in range(len(low)):
-        data.append(random.randint(bound[0][pos], bound[1][pos]))
-    gene = Gene(data)
-    pop.append(gene)
+with ThreadPoolExecutor(max_workers=100) as t:
+    obj_list = []
+    for _ in range(pop_size - 2):
+        obj = t.submit(get_valid_gene)
+        obj_list.append(obj)
+    for obj in as_completed(obj_list):
+        result = obj.result()
+        pop.append(result)
 get_fitness(pop)
 e = time.time()
 print(e - s)
 pop.sort(reverse=True)
-
-
