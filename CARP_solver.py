fix the bug for capacity overflow

CARP_solver.py
@@ -87,7 +87,8 @@ def probability(new_val, old_val, T):
     return math.exp((old_val - new_val) / T)
 
 
-def flip(routes, costs, route_demands, depot, best_routes, best_costs):
+def flip(routes, costs, route_demands, depot, best_routes, best_costs, best_demands):
+    tmp = copy.deepcopy(routes)
     for l in range(len(routes)):
         route = routes[l]
         for i in range(len(route)):
@@ -108,10 +109,24 @@ def flip(routes, costs, route_demands, depot, best_routes, best_costs):
                 if current_cost < sum(best_costs):
                     best_costs = copy.deepcopy(costs)
                     best_routes = copy.deepcopy(routes)
-    return routes, costs, route_demands, best_routes, best_costs
+                    best_demands = copy.deepcopy(route_demands)
+    #
+    # for i in range(len(best_routes)):
+    #     route = best_routes[i]
+    #     dd = 0
+    #     for r in route:
+    #         dd += demands[r[0]][r[1]]
+    #     if best_demands[i] != dd:
+    #         for j in range(len(tmp)):
+    #             if tmp[j] != best_routes[j]:
+    #                 print(tmp[j])
+    #                 print(best_routes[j])
+    #         print(i, "flip")
+    #         print(best_demands[i], dd)
+    return routes, costs, route_demands, best_routes, best_costs, best_demands
 
 
-def self_swap(routes, costs, route_demands, depot, best_routes, best_costs):
+def self_swap(routes, costs, route_demands, depot, best_routes, best_costs, best_demands):
     for l in range(len(routes)):
         route = routes[l]
         if len(route) == 1 or random.random() < 0.5:
@@ -158,56 +173,88 @@ def self_swap(routes, costs, route_demands, depot, best_routes, best_costs):
         if current_cost < sum(best_costs):
             best_costs = copy.deepcopy(costs)
             best_routes = copy.deepcopy(routes)
-    return routes, costs, route_demands, best_routes, best_costs
+            best_demands = copy.deepcopy(route_demands)
+    #
+    # for i in range(len(best_routes)):
+    #     route = best_routes[i]
+    #     dd = 0
+    #     for r in route:
+    #         dd += demands[r[0]][r[1]]
+    #     if best_demands[i] != dd:
+    #         print(i, "self-swap")
+    #         print(best_demands[i], dd)
+    return routes, costs, route_demands, best_routes, best_costs, best_demands
 
 
-def cross_swap(routes, costs, route_demands, depot, best_routes, best_costs):
+def cross_swap(routes, costs, route_demands, depot, best_routes, best_costs, best_demands):
+    tmp = copy.deepcopy(best_demands)
+    best_demands = copy.deepcopy(best_demands)
     if len(routes) == 1:
-        return routes, costs, route_demands, best_routes, best_costs
+        return routes, costs, route_demands, best_routes, best_costs, best_demands
     num = 0
-    while num < 5:
+    while num < 1:
+        # if best_routes != tmp:
+        #     print("changes")
+        #     for i in range(len(best_routes)):
+        #         route = best_routes[i]
+        #         dd = 0
+        #         for r in route:
+        #             dd += demands[r[0]][r[1]]
+        #         if route_demands[i] != dd:
+        #             print(i, "best")
+        #             print(route_demands[i], dd)
         num += 1
         r1 = 0
         r2 = 0
         while r1 == r2:
             r1 = random.randint(0, len(routes) - 1)
             r2 = random.randint(0, len(routes) - 1)
-        route1 = routes[r1]
-        route2 = routes[r2]
-        if len(route1) == 1 and len(route2) == 1:
+        if len(routes[r1]) == 1 and len(routes[r2]) == 1:
             continue
         cnt = 0
         while True:
             cnt += 1
-            pos1 = random.randint(0, len(route1) - 1)
-            pos2 = random.randint(0, len(route2) - 1)
-            seg1 = route1[pos1]
-            seg2 = route2[pos2]
             demand1, demand2 = route_demands[r1], route_demands[r2]
-            demand1 = demand1 - demands[seg1[0]][seg1[1]] + demands[seg2[0]][seg2[1]]
-            demand2 = demand2 - demands[seg2[0]][seg2[1]] + demands[seg1[0]][seg1[1]]
-            if cnt >= len(route1) + len(route2):
+            pos1 = random.randint(0, len(routes[r1]) - 1)
+            pos2 = random.randint(0, len(routes[r2]) - 1)
+            seg1 = routes[r1][pos1]
+            seg2 = routes[r2][pos2]
+            new_demand1 = demand1 - demands[seg1[0]][seg1[1]] + demands[seg2[0]][seg2[1]]
+            new_demand2 = demand2 - demands[seg2[0]][seg2[1]] + demands[seg1[0]][seg1[1]]
+            if cnt >= len(routes[r1]) + len(routes[r2]):
                 break
-            if demand1 <= capacity and demand2 <= capacity:
+            if new_demand1 <= capacity and new_demand2 <= capacity:
+                # for i in range(len(routes)):
+                #     route = routes[i]
+                #     dd = 0
+                #     for r in route:
+                #         dd += demands[r[0]][r[1]]
+                #     if route_demands[i] != dd:
+                #         print(i, "i")
+                #         print(route_demands[i], dd)
+                routes[r1][pos1], routes[r2][pos2] = seg2, seg1
+                # d = 0
+                # for r in routes[r1]:
+                #     d += demands[r[0]][r1]
+                # if new_demand1 != d:
+                #     print("eeeeeeeeeee1")
+                route_demands[r1], route_demands[r2] = new_demand1, new_demand2
                 if pos1 == 0:
                     start1 = depot
                 else:
-                    start1 = route1[pos1 - 1][1]
-                if pos1 == len(route1) - 1:
+                    start1 = routes[r1][pos1 - 1][1]
+                if pos1 == len(routes[r1]) - 1:
                     end1 = depot
                 else:
-                    end1 = route1[pos1 + 1][0]
+                    end1 = routes[r1][pos1 + 1][0]
                 if pos2 == 0:
                     start2 = depot
                 else:
-                    start2 = route2[pos2 - 1][1]
-                if pos2 == len(route2) - 1:
+                    start2 = routes[r2][pos2 - 1][1]
+                if pos2 == len(routes[r2]) - 1:
                     end2 = depot
                 else:
-                    end2 = route2[pos2 + 1][0]
-                route1[pos1], route2[pos2] = seg2, seg1
-                route_demands[r1] = demand1
-                route_demands[r2] = demand2
+                    end2 = routes[r2][pos2 + 1][0]
                 costs[r1] = costs[r1] - distances[start1][seg1[0]] - distances[seg1[1]][end1] - \
                             graph[seg1[0]][seg1[1]] + distances[start1][seg2[0]] + distances[seg2[1]][end1] + \
                             graph[seg2[0]][seg2[1]]
@@ -218,7 +265,40 @@ def cross_swap(routes, costs, route_demands, depot, best_routes, best_costs):
                 if current_cost < sum(best_costs):
                     best_costs = copy.deepcopy(costs)
                     best_routes = copy.deepcopy(routes)
-    return routes, costs, route_demands, best_routes, best_costs
+                    best_demands = copy.deepcopy(route_demands)
+                    for i in range(len(best_routes)):
+                        route = best_routes[i]
+                        dd = 0
+                        for r in route:
+                            dd += demands[r[0]][r[1]]
+                        # if dd > capacity:
+                        if best_demands[i] != dd:
+                            print("flag")
+                            print(cnt)
+                            print(i)
+                            print(r1)
+                            print(r2)
+                            print(route_demands[i])
+                            print(dd)
+                            print(pos1)
+                            print(pos2)
+                            print(seg1)
+                            print(seg2)
+                            print(routes[r1])
+                            print(routes[r2])
+                            exit(0)
+                break
+    for i in range(len(best_routes)):
+        route = best_routes[i]
+        dd = 0
+        for r in route:
+            dd += demands[r[0]][r[1]]
+        if best_demands[i] != dd:
+            if best_demands != tmp:
+                print("alreadychange")
+            print(i, "finalbest")
+            print(best_demands[i], dd)
+    return routes, costs, route_demands, best_routes, best_costs, best_demands
 
 
 class RuleThread(threading.Thread):
@@ -290,10 +370,12 @@ class RuleThread(threading.Thread):
         costs = copy.deepcopy(self.costs)
         route_demands = copy.deepcopy(self.route_demands)
         repeat = 0
-        last_routes = routes
+        last_routes = copy.deepcopy(routes)
         last_cost = sum(costs)
         weight = [0.3, 0.6, 1]
+        num = 0
         while time.time() - start_time <= termination - 2:
+            num += 1
             random_num = random.random()
             if random_num < weight[0]:
                 operation = flip
@@ -301,27 +383,64 @@ class RuleThread(threading.Thread):
                 operation = self_swap
             else:
                 operation = cross_swap
-            temp_routes, temp_costs, temp_route_demands, self.routes, self.costs = operation(routes, costs,
-                                                                                             route_demands,
-                                                                                             self.depot, self.routes,
-                                                                                             self.costs)
+            for i in range(len(routes)):
+                route = routes[i]
+                dd = 0
+                for r in route:
+                    dd += demands[r[0]][r[1]]
+                if route_demands[i] != dd:
+                    print(T)
+                    print("iter", num)
+                    print(i, "iiii")
+                    print(route_demands[i], dd)
+            temp_routes, temp_costs, temp_route_demands, \
+            best_routes, best_costs, best_route_demands = operation(routes, costs,
+                                                                    route_demands,
+                                                                    self.depot,
+                                                                    self.routes,
+                                                                    self.costs,
+                                                                    self.route_demands)
+            self.routes = copy.deepcopy(best_routes)
+            self.costs = copy.deepcopy(best_costs)
+            self.route_demands = copy.deepcopy(best_route_demands)
+            for i in range(len(self.routes)):
+                route = self.routes[i]
+                dd = 0
+                for r in route:
+                    dd += demands[r[0]][r[1]]
+                if self.route_demands[i] != dd:
+                    print(operation)
+                    print(i, "self.routes")
+                    print(self.route_demands[i], dd)
+            temp_routes, temp_costs, temp_route_demands = \
+                copy.deepcopy(temp_routes), copy.deepcopy(temp_costs), copy.deepcopy(temp_route_demands)
             if probability(sum(temp_costs), sum(costs), T) > random.random():
-                routes = temp_routes
-                costs = temp_costs
-                route_demands = temp_route_demands
+                routes = copy.deepcopy(temp_routes)
+                costs = copy.deepcopy(temp_costs)
+                route_demands = copy.deepcopy(temp_route_demands)
             if last_cost == sum(costs) or routes == last_routes:
                 repeat += 1
             else:
                 last_cost = sum(costs)
-                last_routes = routes
+                last_routes = copy.deepcopy(routes)
             if sum(costs) > 1.2 * sum(self.costs):
                 T = 10000
                 routes = copy.deepcopy(self.routes)
                 costs = copy.deepcopy(self.costs)
-                last_routes = self.routes
+                route_demands = copy.deepcopy(self.route_demands)
+                last_routes = copy.deepcopy(self.routes)
+                for i in range(len(routes)):
+                    route = routes[i]
+                    dd = 0
+                    for r in route:
+                        dd += demands[r[0]][r[1]]
+                    if route_demands[i] != dd:
+                        print("epoch", num)
+                        print(i, "update")
+                        print(route_demands[i], dd)
                 continue
             if repeat > 100:
-                last_routes = routes
+                last_routes = copy.deepcopy(routes)
                 T = 10000
                 repeat = 0
                 continue
@@ -401,19 +520,19 @@ capacity = int(params['CAPACITY'])
 
 thread_list = []
 t1 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule1")
-t2 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule2")
-t3 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule3")
-t4 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule4")
-t5 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule5")
-t6 = RandomThread(copy.deepcopy(edges), depot, capacity)
-t7 = RandomThread(copy.deepcopy(edges), depot, capacity)
+# t2 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule2")
+# t3 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule3")
+# t4 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule4")
+# t5 = RuleThread(copy.deepcopy(edges), depot, capacity, "rule5")
+# t6 = RandomThread(copy.deepcopy(edges), depot, capacity)
+# t7 = RandomThread(copy.deepcopy(edges), depot, capacity)
 thread_list.append(t1)
-thread_list.append(t2)
-thread_list.append(t3)
-thread_list.append(t4)
-thread_list.append(t5)
-thread_list.append(t6)
-thread_list.append(t7)
+# thread_list.append(t2)
+# thread_list.append(t3)
+# thread_list.append(t4)
+# thread_list.append(t5)
+# thread_list.append(t6)
+# thread_list.append(t7)
 
 final_cost = np.inf
 final_routes = []
@@ -427,3 +546,8 @@ for t in thread_list:
         final_routes = t.routes
 
 print_info(final_routes, final_cost)
+for route in final_routes:
+    dd = 0
+    for r in route:
+        dd += demands[r[0]][r[1]]
+    print(dd)