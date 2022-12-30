69b670958e22eab7cf94a13c6c5e87b3460f32c8
SnowCharm <619022098@qq.com>
2022-12-30 11:05:11 +0800

    update training result

--- Trajectory Planning/train.py
+++ Trajectory Planning/train.py
@@ -29,7 +29,7 @@ class Net(nn.Module):
 data = torch.load('data.pth')
 feature = data['feature']
 label = data['label']
-X_train, test_data, y_train, test_label = train_test_split(feature, label, test_size=0.1, random_state=42)
+X_train, test_data, y_train, test_label = train_test_split(feature, label, test_size=0.1)
 
 input_size = int(feature.shape[1])
 output_size = int(torch.unique(label).shape[0])
@@ -40,8 +40,8 @@ criterion = nn.CrossEntropyLoss()
 
 epochs = 500
 best_accuracy = 0
+train_data, val_data, train_label, val_label = train_test_split(X_train, y_train, test_size=0.1)
 for epoch in trange(epochs):
-    train_data, val_data, train_label, val_label = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
     train_output = net(train_data)
     loss = criterion(train_output, train_label)
 
