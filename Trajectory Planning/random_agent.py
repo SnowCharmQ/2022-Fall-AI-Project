7d079c277dd98b1dce31ba516fa42e64ee38d880
SnowCharm <619022098@qq.com>
2023-01-06 20:28:19 +0800

    update random agent implementation

--- Trajectory Planning/random_agent.py
+++ Trajectory Planning/random_agent.py
@@ -2,11 +2,74 @@ import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from typing import Tuple
-from src import *
 
+P = 3
+N_CTPS = 5
+RADIUS = 0.3
 input_size = 256
 output_size = 10
+
+
+def evaluate(
+        traj: torch.Tensor,
+        target_pos: torch.Tensor,
+        target_scores: torch.Tensor,
+        radius: float,
+) -> torch.Tensor:
+    cdist = torch.cdist(target_pos, traj)
+    d = cdist.min(-1).values
+    hit = (d < radius)
+    value = torch.sum(hit * target_scores, dim=-1)
+    return value
+
+
+def compute_traj(ctps_inter: torch.Tensor):
+    """Compute the discretized trajectory given the second to the second control points"""
+    t = torch.linspace(0, N_CTPS - P, 100, device=ctps_inter.device)
+    knots = torch.cat([
+        torch.zeros(P, device=ctps_inter.device),
+        torch.arange(N_CTPS + 1 - P, device=ctps_inter.device),
+        torch.full((P,), N_CTPS - P, device=ctps_inter.device),
+    ])
+    ctps = torch.cat([
+        torch.tensor([[0., 0.]], device=ctps_inter.device),
+        ctps_inter,
+        torch.tensor([[N_CTPS, 0.]], device=ctps_inter.device)
+    ])
+    return splev(t, knots, ctps, P)
+
+
+def splev(
+        x: torch.Tensor,
+        knots: torch.Tensor,
+        ctps: torch.Tensor,
+        degree: int,
+        der: int = 0
+) -> torch.Tensor:
+    if der == 0:
+        return _splev_torch_impl(x, knots, ctps, degree)
+    else:
+        assert der <= degree, "The order of derivative to compute must be less than or equal to k."
+        n = ctps.size(-2)
+        ctps = (ctps[..., 1:, :] - ctps[..., :-1, :]) / (knots[degree + 1:degree + n] - knots[1:n]).unsqueeze(-1)
+        return degree * splev(x, knots[..., 1:-1], ctps, degree - 1, der - 1)
+
+
+def _splev_torch_impl(x: torch.Tensor, t: torch.Tensor, c: torch.Tensor, k: int):
+    assert t.size(0) == c.size(0) + k + 1, f"{len(t)} != {len(c)} + {k} + {1}"  # m= n + k + 1
+    x = torch.atleast_1d(x)
+    assert x.dim() == 1 and t.dim() == 1 and c.dim() == 2, f"{x.shape}, {t.shape}, {c.shape}"
+    n = c.size(0)
+    u = (torch.searchsorted(t, x) - 1).clip(k, n - 1).unsqueeze(-1)
+    x = x.unsqueeze(-1)
+    d = c[u - k + torch.arange(k + 1, device=c.device)].contiguous()
+    for r in range(1, k + 1):
+        j = torch.arange(r - 1, k, device=c.device) + 1
+        t0 = t[j + u - k]
+        t1 = t[j + u + 1 - r]
+        alpha = ((x - t0) / (t1 - t0)).unsqueeze(-1)
+        d[:, j] = (1 - alpha) * d[:, j - 1] + alpha * d[:, j]
+    return d[:, k]
 
 
 class Net(nn.Module):
@@ -35,18 +98,6 @@ class RandomAgent:
         self.classifier = Net(input_size, int(input_size * 1.5), int(input_size * 1.5), output_size)
         self.classifier.load_state_dict(torch.load("model.pkl"))
 
-    def evaluate(self, traj: torch.Tensor,
-                 target_pos: torch.Tensor,
-                 target_scores: torch.Tensor,
-                 radius: float) -> torch.Tensor:
-        cdist = torch.cdist(target_pos, traj)
-        d = cdist.min(-1).values
-        hits = d <= radius
-        d[hits] = 1
-        d[~hits] = radius / d[~hits]
-        value = torch.sum(d * target_scores, dim=-1)
-        return value
-
     def get_action(self,
                    target_pos: torch.Tensor,
                    target_features: torch.Tensor,
@@ -56,15 +107,12 @@ class RandomAgent:
         target_class_pred = torch.max(target_class_pred, 1)[1]
         target_class_pred = target_class_pred.data.numpy()
 
-        ctps_inter = torch.rand((N_CTPS - 2, 2)) * torch.tensor([N_CTPS - 2, 2.]) + torch.tensor([1., -1.])
-        ctps_inter.requires_grad = True
-        best_score = -100
+        ctps_inter = torch.Tensor([[-100, -100], [-100, -100], [-100, 100]])
+        best_score = torch.tensor(0)
         while not time.time() - s > 0.29:
             temp = torch.rand((N_CTPS - 2, 2)) * torch.tensor([N_CTPS - 2, 2.]) + torch.tensor([1., -1.])
             score = evaluate(compute_traj(temp), target_pos, class_scores[target_class_pred], RADIUS)
             if score > best_score:
                 best_score = score
                 ctps_inter = temp
-        if best_score < 0:
-            ctps_inter = torch.Tensor([[-100, -100], [-100, -100], [-100, 100]])
         return ctps_inter
